b7705d57f4ce75b0b8513f16dc81b598d222c3d4
Vladyslav Sikailo <dren7777@gmail.com>
2018-12-25 21:55:38 +0200

    fixed login validation error

--- FileSharing/ViewModels/LoginModel.cs
+++ FileSharing/ViewModels/LoginModel.cs
@@ -4,7 +4,7 @@ namespace FileSharing.ViewModels
 {
     public class LoginModel
     {
-        [Required(ErrorMessage = "Не указан email")]
+        [Required(ErrorMessage = "Не указан Email")]
         public string Email { get; set; }
 
         [Required(ErrorMessage = "Не указан пароль")]
